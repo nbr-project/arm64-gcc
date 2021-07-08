c7e99568108c6a7307b17a9abf738df37dc0d9d0
KangMamles <imzea69@gmail.com>
2021-07-08 13:30:41 +0000

    Import GCC 11.1.0, Binutils 2.36.1
    Signed-off-by: KangMamles <imzea69@gmail.com>

---
+++ lib/gcc/aarch64-elf/11.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/__w/build-gcc/build-gcc/gcc/configure CFLAGS='-g0 -O2 -fstack-protector-strong' CXXFLAGS='-g0 -O2 -fstack-protector-strong' --target=aarch64-elf --enable-targets=all --prefix=/__w/build-gcc/build-gcc/arm64-gcc --enable-languages=c --without-headers --disable-bootstrap --disable-nls --disable-threads --disable-shared --disable-libmudflap --disable-libssp --disable-libgomp --disable-decimal-float --disable-libquadmath --disable-libatomic --disable-libcc1 --disable-libmpx --enable-checking=release --with-newlib";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
